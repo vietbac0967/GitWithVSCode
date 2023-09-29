3dd3e57fe4a7579387d0935f4592e23809eb963e
VietBac <81665443+vietbac0967@users.noreply.github.com>
2023-09-29 17:31:46 +0700

    add screen home

---
+++ screens/ScreenHome.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+export default function ScreenHome() {
+  return (
+    <View>
+      <Text>ScreenHome</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
